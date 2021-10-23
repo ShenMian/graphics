b16d5517945ecc40dbfb599801e709727333581e
ShenMian <sms_school@outlook.com>
2021-10-23 23:58:03 +0800

    Added Scene

---
+++ Source/Scene.cpp
@@ -0,0 +1,34 @@
+﻿// Copyright 2021 SMS
+// License(Apache-2.0)
+
+#include "Scene.h"
+#include <assimp/Exporter.hpp>
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <cassert>
+
+namespace fs = std::filesystem;
+
+void Scene::load(const fs::path& path)
+{
+    Assimp::Importer importer;
+
+    // 从文件导入场景数据
+    auto scene = importer.ReadFile(path.string(),
+                                   aiProcess_CalcTangentSpace |
+                                       aiProcess_Triangulate |
+                                       aiProcess_JoinIdenticalVertices |
+                                       aiProcess_SortByPType);
+
+    assert(scene != nullptr);
+}
+
+void Scene::save(const fs::path& path)
+{
+    assert(scene != nullptr);
+
+    Assimp::Exporter exporter;
+
+    // 导出场景数据到文件
+    exporter.Export(scene, path.extension().string(), path.string());
+}
---
+++ Source/Scene.h
@@ -0,0 +1,27 @@
+﻿// Copyright 2021 SMS
+// License(Apache-2.0)
+
+#pragma once
+
+#include <filesystem>
+#include <unordered_map>
+#include <assimp/scene.h>
+
+/**
+ * @brief 3D 模型.
+ */
+class Scene
+{
+public:
+    void load(const std::filesystem::path& path);
+    void save(const std::filesystem::path& path);
+
+private:
+    aiScene* scene = nullptr;
+};
+
+/*
+inline static std::vector<std::string> extensions = {
+    "collada", "x", "stp", "obj", "objnomtl", "stl", "stlb", "ply", "plyb", "3ds",
+    "gltf2", "glb2", "gltf", "glb", "assbin", "assxml", "x3d", "3mf", "pbrt", "assjson"};
+*/
