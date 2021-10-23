92257544bb34031a59fc45eb3ec8e00902198d83
ShenMian <sms_school@outlook.com>
2021-10-23 21:06:44 +0800

    Fixed generate_vs2019_project.bat

--- Scripts/generate_vs2019_project.bat
+++ Scripts/generate_vs2019_project.bat
@@ -4,13 +4,13 @@ REM License(Apache-2.0)
 
 REM 切换到主目录
 pushd %~dp0\..
-if not exist Math.sln set firstGenerate=true
+if not exist Graphics.sln set firstGenerate=true
 
 REM 生成VS解决方案
 Thirdparty\premake\bin\premake5.exe vs2019
 if errorlevel 1 pause
 
 REM 打开解决方案
-if "%firstGenerate%"=="true" start Math.sln
+if "%firstGenerate%"=="true" start Graphics.sln
 
 popd
