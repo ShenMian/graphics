62f18976103f5b196cd8641a23eeebd2ba15042d
ShenMian <sms_school@outlook.com>
2021-10-24 20:57:51 +0800

    Added Renderer

---
+++ Source/Renderer.h
@@ -0,0 +1,46 @@
+﻿// Copyright 2021 SMS
+// License(Apache-2.0)
+
+#pragma once
+
+namespace clem
+{
+
+/**
+ * @addtogroup Rendering
+ * @{
+ */
+
+class Renderer
+{
+public:
+    enum class API
+    {
+        OpenGL,
+        Vulkan,
+        D3D12,
+        Metal
+    };
+
+    /**
+	 * @brief 设置图形 API.
+	 *
+	 * @param api 图形 API.
+	 */
+    static void setAPI(API api);
+
+    /**
+	 * @brief 获取图形 API.
+	 */
+    static API getAPI();
+
+private:
+    static API api;
+};
+
+/**
+ * end of Rendering group
+ * @}
+ */
+
+} // namespace clem
