7664aa2c608c301cd2abeaaf1830c4c9e2222798
ShenMian <sms_school@outlook.com>
2021-11-14 00:34:34 +0800

    Update scripts

--- Scripts/gen_gmake2.bat
+++ Scripts/gen_gmake2.bat
@@ -5,6 +5,9 @@ REM License(Apache-2.0)
 REM 切换到主目录
 pushd %~dp0\..
 
+REM 签出第三方库
+git submodule update --init
+
 ThirdParty\premake\bin\premake5.exe gmake2
 
 popd
--- Scripts/gen_gmake2.sh
+++ Scripts/gen_gmake2.sh
@@ -6,4 +6,7 @@
 cd "$( cd "$( dirname "$0"  )" && pwd  )" || exit
 cd ..
 
+# 签出第三方库
+git submodule update --init
+
 ./ThirdParty/premake/bin/premake5 gmake2
