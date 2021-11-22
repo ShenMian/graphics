0753fa60e18de88fc1309d30d26d30392f3b38b2
ShenMian <sms_school@outlook.com>
2021-11-22 22:54:40 +0800

    Update scripts

--- Scripts/cmake_build.bat
+++ Scripts/cmake_build.bat
@@ -2,8 +2,16 @@
 REM Copyright 2021 ShenMian
 REM License(Apache-2.0)
 
-if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" >NUL
-if exist "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvars64.bat" call "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvars64.bat" >NUL
+if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" (
+    call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" >NUL
+) else (
+    if exist "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvars64.bat" (
+        call "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvars64.bat" >NUL
+    ) else (
+        echo Can't find vcvars64.bat
+        exit /b 1
+    )
+)
 
 REM 切换到主目录
 pushd %~dp0\..
@@ -13,24 +21,22 @@ call Scripts/cmake_install_deps || exit /b 1
 
 REM 創建 build 目錄
 if not exist build mkdir build
-pushd build
 
 REM 生成 CMake 緩存
 echo Generating CMake cache...
-cmake .. >NUL || (
+cmake -B build >NUL || (
     echo Failed to generate CMake cache.
     exit /b 1
 )
 
 REM 構建
 echo Building...
-cmake --build . >NUL || (
+cmake --build build >NUL || (
     echo Failed to build.
+    cmake --build build
     exit /b 1
 )
 
-echo Done
-
-popd
+echo Done.
 
 popd
--- Scripts/cmake_install_deps.bat
+++ Scripts/cmake_install_deps.bat
@@ -2,6 +2,7 @@
 REM Copyright 2021 ShenMian
 REM License(Apache-2.0)
 
+REM 检测是否有管理员权限
 net.exe session 1>NUL 2>NUL || (
     echo Please run this script with administrator privilege.
     exit /b 1
@@ -10,7 +11,11 @@ net.exe session 1>NUL 2>NUL || (
 pushd %~dp0\..\ThirdParty
 
 REM 签出第三方库
-git submodule update --init
+echo Checkout third-party libraries...
+git submodule update --init >NUL || (
+    echo Failed to checkout third-party libraries.
+    exit /b 1
+)
 
 echo Installing dependencies...
 
@@ -21,30 +26,29 @@ for %%i in ("assimp" "glfw" "meshoptimizer") do (
 
     REM 創建 build 目錄
     if not exist build mkdir build
-    cd build
 
     REM 生成 CMake 緩存
     echo  ^|-Gerenating CMake cache...
-    cmake .. >NUL || (
+    cmake -B build >NUL || (
         echo  ^|-Failed to generate CMake cache.
         exit /b 1
     )
 
     REM 构建
     echo  ^|-Building...
-    cmake --build . --config Release >NUL || (
+    cmake --build build --config Release >NUL || (
         echo  ^|-Failed to build.
         exit /b 1
     )
 
     REM 安装
     echo  ^|-Installing...
-    cmake --install . >NUL || (
+    cmake --install build >NUL || (
         echo  ^|-Failed to install.
         exit /b 1
     )
 
-    echo  ^|-Done
+    echo  ^|-Done.
 
     popd
 )
