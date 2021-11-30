bf3180c5415d940e08f0f86735f2763fe7c36d49
ShenMian <sms_school@outlook.com>
2021-11-30 22:20:02 +0800

    Format code style

--- Examples/Test/main.cpp
+++ Examples/Test/main.cpp
@@ -3,52 +3,53 @@
 
 #include "Graphics.h"
 
-struct Vertex
-{
-	Vector2 position;
-	Vector3 color;
-};
+void PrintInfo();
 
 int main()
 {
 	Window::init();
 
-    auto window = new Window("Example", Monitor::getPrimary().getSize() / 2);
+	auto window = new Window("Example", Monitor::getPrimary().getSize() / 2);
 	window->setVisible(true); // 设置窗口可见
 
-	auto forword = Program::create("Shaders/forword"); // 从源文件创建着色器程序
+	Renderer::setAPI(Renderer::API::OpenGL); // 设置渲染 API 为 OpenGL
+
+	PrintInfo();
 
 	Model model;
 	model.load("../../../3DModel/scene/Crytek_Sponza/sponza.obj");
 	// model.load("../../../3DModel/scene/SunTemple/SunTemple.fbx"); // 暂不支持格式的压缩纹理资源
 	// model.load("../../../3DModel/weapon/m4a1/m4a1.gltf");
 
-	auto cmdQueue  = CommandQueue::create();
+	auto program = Program::create("Shaders/forword");
+	auto cmdQueue = CommandQueue::create();
 	auto cmdBuffer = CommandBuffer::create();
 
-	bool running    = true;
+	bool running = true;
 	window->onClose = [&]() { running = false; };
-    window->onKey   = [&](int action, Key key)
-    {
-        if (action == 1)
-        {
-            switch (key)
-            {
-            case Key::ESCAPE:
-                running = false;
-                break;
+	window->onKey = [&](int action, Key key)
+	{
+		if(action == 1)
+		{
+			switch(key)
+			{
+			case Key::ESCAPE:
+				running = false;
+				break;
 
-            case Key::F11:
-                window->setFullscreen(!window->isFullscreen());
-                break;
-            }
-        }
-    };
+			case Key::F11:
+				window->setFullscreen(!window->isFullscreen());
+				break;
+			}
+		}
+	};
+
 	while(running)
 	{
-		forword->use();
+		program->use();
 		cmdBuffer->begin();
 		{
+			cmdBuffer->setViewport({0, 0}, window->getSize());
 			cmdBuffer->setClearColor({0, 0, 0, 0});
 			cmdBuffer->clear(ClearFlag::Color);
 
@@ -61,15 +62,35 @@ int main()
 				cmdBuffer->setIndexBuffer(ibo);
 				cmdBuffer->draw(0, vbo->getCount());
 			}
+
 		}
 		cmdBuffer->end();
 		cmdQueue->submit(cmdBuffer);
 
 		window->update();
 	}
-    delete window;
+	delete window;
 
-    Window::deinit();
+	Window::deinit();
 
 	return 0;
+}
+
+void PrintInfo()
+{
+	// 打印显示器信息
+	for(const auto& mon : Monitor::getMonitors())
+	{
+		puts("Monitor");
+		puts(std::format("|-Name        : {}", mon.getName()).c_str());
+		puts(std::format("|-Size        : {}x{}", mon.getSize().x, mon.getSize().y).c_str());
+		puts(std::format("`-Refresh rate: {} Hz", mon.getRefreshRate()).c_str());
+	}
+
+	// 打印基本信息
+	const auto renderer = Renderer::get();
+	puts("Basic");
+	puts(std::format("|-Device  : {}", renderer->getDeviceName()).c_str());
+	puts(std::format("|-Renderer: {}", renderer->getRendererName()).c_str());
+	puts(std::format("`-Vendor  : {}", renderer->getVendorName()).c_str());
 }
--- Examples/Triangle/main.cpp
+++ Examples/Triangle/main.cpp
@@ -15,66 +15,66 @@ int main()
 {
 	Window::init();
 
-    auto window = new Window("Triangle", Monitor::getPrimary().getSize() / 2);
-    window->setVisible(true); // 设置窗口可见
+	auto window = new Window("Triangle", Monitor::getPrimary().getSize() / 2);
+	window->setVisible(true); // 设置窗口可见
 
-    Renderer::setAPI(Renderer::API::OpenGL); // 设置渲染 API 为 OpenGL
+	Renderer::setAPI(Renderer::API::OpenGL); // 设置渲染 API 为 OpenGL
 
-    PrintInfo();
+	PrintInfo();
 
-    const std::vector<Vertex> vertices = {
-        {{0, 0.5}, {1, 0, 0}},
-        {{0.5, -0.5}, {0, 1, 0}},
-        {{-0.5, -0.5}, {0, 0, 1}}
-    };
+	const std::vector<Vertex> vertices = {
+		{{0, 0.5}, {1, 0, 0}},
+		{{0.5, -0.5}, {0, 1, 0}},
+		{{-0.5, -0.5}, {0, 0, 1}}
+	};
 
-    VertexFormat format = {
-        {"position", Format::RG32F},
-        {"color", Format::RGB32F}
-    };
-    format.setStride(sizeof(Vertex));
+	VertexFormat format = {
+		{"position", Format::RG32F},
+		{"color", Format::RGB32F}
+	};
+	format.setStride(sizeof(Vertex));
 
-    auto vbo       = VertexBuffer::create(vertices, format);
-    auto program   = Program::create("Shaders/forword");
-    auto cmdQueue  = CommandQueue::create();
-    auto cmdBuffer = CommandBuffer::create();
+	auto vbo = VertexBuffer::create(vertices, format);
+	auto program = Program::create("Shaders/forword");
+	auto cmdQueue = CommandQueue::create();
+	auto cmdBuffer = CommandBuffer::create();
 
-    bool running    = true;
-    window->onClose = [&]() { running = false; };
-    window->onKey   = [&](int action, Key key)
-    {
-        if (action == 1)
-        {
-            switch (key)
-            {
-            case Key::ESCAPE:
-                running = false;
-                break;
+	bool running = true;
+	window->onClose = [&]() { running = false; };
+	window->onKey = [&](int action, Key key)
+	{
+		if(action == 1)
+		{
+			switch(key)
+			{
+			case Key::ESCAPE:
+				running = false;
+				break;
 
-            case Key::F11:
-                window->setFullscreen(!window->isFullscreen());
-                break;
-            }
-        }
-    };
+			case Key::F11:
+				window->setFullscreen(!window->isFullscreen());
+				break;
+			}
+		}
+	};
 
-    while(running)
-    {
-        program->use();
-        cmdBuffer->begin();
-        {
-            cmdBuffer->setViewport({0, 0}, window->getSize());
-            cmdBuffer->setClearColor({0, 0, 0, 0});
-            cmdBuffer->clear(ClearFlag::Color);
-            cmdBuffer->setVertexBuffer(vbo);
-            cmdBuffer->draw(0, vbo->getCount());
-        }
-        cmdBuffer->end();
-        cmdQueue->submit(cmdBuffer);
+	while(running)
+	{
+		program->use();
+		cmdBuffer->begin();
+		{
+			cmdBuffer->setViewport({0, 0}, window->getSize());
+			cmdBuffer->setClearColor({0, 0, 0, 0});
+			cmdBuffer->clear(ClearFlag::Color);
+			cmdBuffer->setVertexBuffer(vbo);
+			cmdBuffer->draw(0, vbo->getCount());
+		}
+		cmdBuffer->end();
+		cmdQueue->submit(cmdBuffer);
 
-        window->update();
-    }
-    delete window;
+		window->update();
+	}
+	delete window;
 
 	Window::deinit();
 
@@ -83,19 +83,19 @@ int main()
 
 void PrintInfo()
 {
-    // 打印显示器信息
-    for(const auto& mon : Monitor::getMonitors())
-    {
-        puts("Monitor");
-        puts(std::format("|-Name        : {}", mon.getName()).c_str());
-        puts(std::format("|-Size        : {}x{}", mon.getSize().x, mon.getSize().y).c_str());
-        puts(std::format("`-Refresh rate: {} Hz", mon.getRefreshRate()).c_str());
-    }
+	// 打印显示器信息
+	for(const auto& mon : Monitor::getMonitors())
+	{
+		puts("Monitor");
+		puts(std::format("|-Name        : {}", mon.getName()).c_str());
+		puts(std::format("|-Size        : {}x{}", mon.getSize().x, mon.getSize().y).c_str());
+		puts(std::format("`-Refresh rate: {} Hz", mon.getRefreshRate()).c_str());
+	}
 
-    // 打印基本信息
-    const auto renderer = Renderer::get();
-    puts("Basic");
-    puts(std::format("|-Device  : {}", renderer->getDeviceName()).c_str());
-    puts(std::format("|-Renderer: {}", renderer->getRendererName()).c_str());
-    puts(std::format("`-Vendor  : {}", renderer->getVendorName()).c_str());
+	// 打印基本信息
+	const auto renderer = Renderer::get();
+	puts("Basic");
+	puts(std::format("|-Device  : {}", renderer->getDeviceName()).c_str());
+	puts(std::format("|-Renderer: {}", renderer->getRendererName()).c_str());
+	puts(std::format("`-Vendor  : {}", renderer->getVendorName()).c_str());
 }
