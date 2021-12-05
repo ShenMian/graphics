e651898ba99264931ba799c44d0982970467865f
ShenMian <sms_school@outlook.com>
2021-12-05 10:48:43 +0800

    Add Input
    Add isPressed() to get the state of mouse button/position and key.

---
+++ Source/Core/Input.cpp
@@ -0,0 +1,43 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#include "Input.h"
+#include "Window.h"
+#include <GLFW/glfw3.h>
+
+Window* Input::window;
+
+bool Input::isPressed(Key key)
+{
+	auto* native = static_cast<GLFWwindow*>(window->getNativeHandle());
+	return glfwGetKey(native, static_cast<int>(key)) == GLFW_PRESS;
+}
+
+bool Input::isPressed(Mouse key)
+{
+	auto* native = static_cast<GLFWwindow*>(window->getNativeHandle());
+	return glfwGetMouseButton(native, static_cast<int>(key)) == GLFW_PRESS;
+}
+
+Vector2 Input::getMousePosition()
+{
+	Vector2d pos;
+	auto* native = static_cast<GLFWwindow*>(window->getNativeHandle());
+	glfwGetCursorPos(native, &pos.x, &pos.y);
+	return Vector2(pos);
+}
+
+float Input::getMouseX()
+{
+	return getMousePosition().x;
+}
+
+float Input::getMouseY()
+{
+	return getMousePosition().y;
+}
+
+void Input::setWindow(Window* win)
+{
+	window = win;
+}
---
+++ Source/Core/Input.h
@@ -0,0 +1,54 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#pragma once
+
+#include "Key.h"
+#include <Math/Vector2.hpp>
+
+class Window;
+
+class Input
+{
+public:
+	/**
+	 * @brief 获取键盘按键状态.
+	 *
+	 * @param  key   键盘按键.
+	 * @return true  按键按下.
+	 * @return false 按键抬起.
+	 */
+	static bool isPressed(Key key);
+
+	/**
+	 * @brief 获取鼠标按键状态.
+	 *
+	 * @param  key   键盘按键.
+	 * @return true  按键按下.
+	 * @return false 按键抬起.
+	 */
+	static bool isPressed(Mouse key);
+
+	/**
+	 * @brief 获取光标相对窗口的位置.
+	 */
+	static Vector2 getMousePosition();
+
+	/**
+	 * @brief 获取光标相对窗口的 X 坐标.
+	 */
+	static float getMouseX();
+
+	/**
+	 * @brief 获取光标相对窗口的 Y 坐标.
+	 */
+	static float getMouseY();
+
+	/**
+	 * @brief 设置接受输入的窗口.
+	 */
+	static void setWindow(Window* win);
+
+private:
+	static Window* window;
+};
