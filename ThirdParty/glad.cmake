31fd88d77243b812539d4211303d08d2273c13ff
ShenMian <sms_school@outlook.com>
2021-12-08 13:48:39 +0800

    Update CMake

--- ThirdParty/glad.cmake
+++ ThirdParty/glad.cmake
@@ -1,3 +1,3 @@
 
-include_directories("${THIRDPARTY_DIR}/glad/include")
-add_library(glad STATIC "${THIRDPARTY_DIR}/glad/src/glad.c")
+include_directories("${DEPS_DIR}/glad/include")
+add_library(glad STATIC "${DEPS_DIR}/glad/src/glad.c")
