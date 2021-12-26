3080afad972dd0916fbd9ccc9e2c1a24d9280daf
ShenMian <sms_school@outlook.com>
2021-12-26 23:41:19 +0800

    Update doxygen comment

--- Source/Graphics.h
+++ Source/Graphics.h
@@ -31,15 +31,47 @@
  * Graphics
  * ========
  *
- * Overview
- * --------
- * A simple abstraction layer for the modern graphics APIs.
+ * 概述
+ * ----
+ * 一个简单的现代图形 API 抽象层.
  *
- * Examples
- * --------
- * [Triangle](https://github.com/ShenMian/Graphics/tree/main/Examples/Triangle)
+ * 文档
+ * ----
+ * - @subpage GettingStarted
  *
  * @defgroup core   核心
  * @defgroup model  模型
  * @defgroup shader 渲染器
+ */
+
+/**
+ * @page GettingStarted 快速入门
+ *
+ * 用法
+ * ----
+ * 通过 CMake 可以很快速/简单的使用该库.
+ *
+ * 1. 将该仓库作为子模块加入你的仓库. 在你的仓库下执行下面的命令.
+ * ```bash
+ * git submodule add https://github.com/ShenMian/Graphics --recursive
+ * ```
+ *
+ * 2. 在 CMakeLists.txt 里加入下面代码. 请根据具体情况做适当修改.
+ * ```cmake
+ * add_subdirectory(Graphics)              # 构建 Graphics 并生成静态库
+ * add_executable(YourApp YourApp.cpp)     # 你的可执行文件, 里面包含了对 Graphics API 的调用
+ * target_link_libraries(YourApp graphics) # 连接 Graphics 静态库, 也可以使用 graphics::graphics
+ * ```
+ *
+ * 通过 Premake 生成你所使用 IDE 的项目文件.
+ * ```bash
+ * ./Scripts/gen_gmake2 # 生成 GNU Makefile
+ * ./Scripts/gen_vs2022 # 生成 Visual Studio 2022 解决方案
+ * ./Scripts/gen_xcode4 # 生成 Xcode4 项目文件
+ * ```
+ * 除此之外, 你还可以自行调用 premake5 生成你想要的内容. 详情请查看[相关文档](https://github.com/premake/premake-core/wiki/Using-Premake#using-premake-to-generate-project-files).
+ *
+ * 示例
+ * ----
+ * - [Triangle](https://github.com/ShenMian/Graphics/tree/main/Examples/Triangle)
  */
