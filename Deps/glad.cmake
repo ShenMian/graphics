e05959ef2ffb32c7a9a144e390ffb72920a605ae
ShenMian <sms_school@outlook.com>
2022-01-01 01:00:35 +0800

    Update CMake

--- Deps/glad.cmake
+++ Deps/glad.cmake
@@ -1,3 +1,3 @@
 
 add_library(glad STATIC "${DEPS_DIR}/glad/src/glad.c")
-target_include_directories(glad PUBLIC "${DEPS_DIR}/glad/include")
+target_include_directories(glad PRIVATE "${DEPS_DIR}/glad/include")
--- Deps/imgui.cmake
+++ Deps/imgui.cmake
@@ -7,7 +7,7 @@ add_library(imgui STATIC
     "${DEPS_DIR}/imgui/imgui_widgets.cpp"
     "${DEPS_DIR}/imgui/backends/imgui_impl_glfw.cpp"
     "${DEPS_DIR}/imgui/backends/imgui_impl_opengl3.cpp")
-target_include_directories(imgui PUBLIC
+target_include_directories(imgui PRIVATE
     "${DEPS_DIR}/imgui"
     "${DEPS_DIR}/glad/include"
     "${DEPS_DIR}/glfw/include"
