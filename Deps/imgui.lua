29181a7ebe2ce20c43c8b24da29089e0845c86b0
ShenMian <sms_school@outlook.com>
2022-01-01 00:38:05 +0800

    Update imgui.lua

--- Deps/imgui.lua
+++ Deps/imgui.lua
@@ -5,24 +5,18 @@ project "imgui"
 	staticruntime "on"
 
 	files {
-        "imgui/imgui/**.h",
-        "imgui/imgui/*.cpp",
+        "imgui/**.h",
+        "imgui/*.cpp",
 
         "imgui/backends/imgui_impl_glfw.cpp",
-        "imgui/backends/imgui_impl_opengl3.cpp"}
-        -- "imgui/backends/imgui_impl_vulkan.cpp"}
+        "imgui/backends/imgui_impl_opengl3.cpp",
+        "imgui/backends/imgui_impl_vulkan.cpp"}
 
 	includedirs {
         "imgui",
 		"%{deps_inc.glad}",
 		"%{deps_inc.glfw}",
 		"%{deps_inc.vulkan}"}
-
-	filter "system:windows"
-		systemversion "latest"
-
-	filter "system:linux"
-		systemversion "latest"
 
 	filter "configurations:Debug"
 		runtime "Debug"
