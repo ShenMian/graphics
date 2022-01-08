451f10a42ad92f4e7e5674981b82379a6900f5b3
ShenMian <sms_school@outlook.com>
2022-01-08 23:23:36 +0800

    Add VKVertexBuffer

---
+++ Source/Vulkan/VKVertexBuffer.cpp
@@ -0,0 +1,18 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#include "VKVertexBuffer.h"
+#include "VKRenderer.h"
+
+VKVertexBuffer::VKVertexBuffer(const void* data, size_t size, size_t count, const VertexFormat& fmt, Usage usage)
+	: VertexBuffer(size, count, fmt)
+{
+}
+
+VKVertexBuffer::~VKVertexBuffer()
+{
+}
+
+void VKVertexBuffer::bind()
+{
+}
---
+++ Source/Vulkan/VKVertexBuffer.h
@@ -0,0 +1,21 @@
+// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#pragma once
+
+#include "VertexBuffer.h"
+#include <vulkan/vulkan.h>
+
+class VKVertexBuffer : public VertexBuffer
+{
+public:
+	using handle_type = VkBuffer;
+
+	VKVertexBuffer(const void* data, size_t size, size_t count, const VertexFormat& fmt, Usage usage);
+	virtual ~VKVertexBuffer();
+
+	void bind() override;
+
+private:
+	handle_type handle;
+};
