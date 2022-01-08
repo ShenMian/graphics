9c610f4bb4ff463587fdad185dcfe3e2d9440ad6
ShenMian <sms_school@outlook.com>
2022-01-08 23:23:24 +0800

    Add VKBuffer

---
+++ Source/Vulkan/VKBuffer.cpp
@@ -0,0 +1,68 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#include "VKBuffer.h"
+
+VKBuffer::VKBuffer(size_t size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties)
+{
+	renderer = reinterpret_cast<VKRenderer*>(Renderer::get());
+	auto& device = renderer->getDevice();
+
+	VkBufferCreateInfo bufInfo = {};
+	bufInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+	bufInfo.size = size;
+	bufInfo.usage = usage;
+	bufInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+	if(vkCreateBuffer(device, &bufInfo, nullptr, &buffer) != VK_SUCCESS)
+		throw std::runtime_error("failed to create buffer");
+
+	VkMemoryRequirements memRequirements;
+	vkGetBufferMemoryRequirements(device, buffer, &memRequirements);
+
+	VkMemoryAllocateInfo allocInfo = {};
+	allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+	allocInfo.allocationSize = memRequirements.size;
+	allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, properties);
+	if(vkAllocateMemory(device, &allocInfo, nullptr, &memory) != VK_SUCCESS)
+		throw std::runtime_error("failed to allocate buffer memory");
+
+	vkBindBufferMemory(device, buffer, memory, 0);
+}
+
+VKBuffer::~VKBuffer()
+{
+	auto& device = renderer->getDevice();
+	vkDestroyBuffer(device, buffer, nullptr);
+	vkFreeMemory(device, memory, nullptr);
+}
+
+VkResult VKBuffer::map(VkDeviceSize size, VkDeviceSize offset)
+{
+	auto& device = renderer->getDevice();
+	return vkMapMemory(device, memory, offset, size, 0, &mapped);
+}
+
+void VKBuffer::unmap()
+{
+	auto& device = renderer->getDevice();
+	if(mapped == nullptr)
+		return;
+	vkUnmapMemory(device, memory);
+	mapped = nullptr;
+}
+
+void* VKBuffer::getMappedMemory()
+{
+	return mapped;
+}
+
+uint32_t VKBuffer::findMemoryType(uint32_t type, VkMemoryPropertyFlags properties)
+{
+	auto& physicalDevice = renderer->getPhysicalDevice();
+	VkPhysicalDeviceMemoryProperties memProperties;
+	vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+	for(uint32_t i = 0; i < memProperties.memoryTypeCount; i++)
+		if((type & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties)
+			return i;
+	throw std::runtime_error("failed to find suitable memory type");
+}
---
+++ Source/Vulkan/VKBuffer.h
@@ -0,0 +1,46 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#pragma once
+
+#include "VKRenderer.h"
+#include <vulkan/vulkan.h>
+
+class VKBuffer
+{
+public:
+	enum class Usage
+	{
+		Static,
+		Dynamic,
+		Stream
+	};
+
+	VKBuffer(size_t size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties);
+	virtual ~VKBuffer();
+
+	/**
+	 * @brief 映射缓冲区到内存.
+	 *
+	 * @param size   要映射的大小, 默认值: 全部.
+	 * @param offset 偏移量, 单位: byte.
+	 * @return VkResult
+	 */
+	VkResult map(VkDeviceSize size = VK_WHOLE_SIZE, VkDeviceSize offset = 0);
+
+	/**
+	 * @brief 取消映射.
+	 */
+	void unmap();
+
+	void* getMappedMemory();
+
+private:
+	uint32_t findMemoryType(uint32_t type, VkMemoryPropertyFlags properties);
+
+	VkBuffer       buffer;
+	VkDeviceMemory memory;
+	void*          mapped = nullptr;
+
+	VKRenderer* renderer;
+};
