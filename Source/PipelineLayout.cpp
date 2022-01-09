f22cf2720942824e1bb7ad5687f4cfaed0b50da2
ShenMian <sms_school@outlook.com>
2022-01-09 18:16:49 +0800

    Add PipelineLayout

---
+++ Source/PipelineLayout.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+
+#include "PipelineLayout.h"
+
+PipelineLayout::PipelineLayout(const std::initializer_list<Attribute>& list)
+	: attribs(list)
+{
+}
+
+const std::vector<PipelineLayout::Attribute> PipelineLayout::getAttributes() const
+{
+	return attribs;
+}
---
+++ Source/PipelineLayout.h
@@ -0,0 +1,53 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#pragma once
+
+#include <cstdint>
+#include <initializer_list>
+#include <vector>
+
+/**
+ * @brief 图形管线绑定布局.
+ */
+class PipelineLayout
+{
+public:
+	enum class Type
+	{
+		Sampler,
+		Texture
+	};
+
+	struct StageFlags
+	{
+		enum
+		{
+			Vertex,
+			Geometry,
+			Fragment,
+			Pixel = Fragment,
+			Compute
+		};
+	};
+
+	struct Attribute
+	{
+		Attribute(Type type, uint32_t slot, int stageFlags, uint32_t arraySize = 1)
+			: type(type), slot(slot), stageFlags(stageFlags), arraySize(arraySize)
+		{
+		}
+
+		Type       type;
+		uint32_t   slot;
+		int        stageFlags;
+		uint32_t   arraySize;
+	};
+
+	PipelineLayout(const std::initializer_list<Attribute>& list);
+
+	const std::vector<Attribute> getAttributes() const;
+
+private:
+	std::vector<Attribute> attribs;
+};
