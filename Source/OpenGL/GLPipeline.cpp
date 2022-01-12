e3ec2db50f6b8d952d4f4eadca84f84e36c3de9b
ShenMian <sms_school@outlook.com>
2022-01-13 01:05:53 +0800

    Add GLPipeline

---
+++ Source/OpenGL/GLPipeline.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#include "GLPipeline.h"
+
+GLPipeline::GLPipeline(const Descriptor& desc)
+	: Pipeline(desc)
+{
+}
---
+++ Source/OpenGL/GLPipeline.h
@@ -0,0 +1,12 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#pragma once
+
+#include "../Pipeline.h"
+
+class GLPipeline : public Pipeline
+{
+public:
+	GLPipeline(const Descriptor& desc);
+};
