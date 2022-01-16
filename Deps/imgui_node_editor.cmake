818d2d70ef53eacbe6c04506af9e180b594114f4
ShenMian <sms_school@outlook.com>
2022-01-16 19:44:45 +0800

    Fix CMake

--- Deps/imgui_node_editor.cmake
+++ Deps/imgui_node_editor.cmake
@@ -1,4 +1,4 @@
-file(GLOB SOURCES "*.cpp")
+file(GLOB SOURCES "${DEPS_DIR}/imgui-node-editor/*.cpp")
 add_library(imgui_node_editor STATIC ${SOURCES})
 target_include_directories(imgui_node_editor PRIVATE
     "${DEPS_DIR}/imgui-node-editor"
