f043ade1dd2a012c58c8c3f2f0e527fa2cef1207
ShenMian <sms_school@outlook.com>
2022-01-21 18:21:39 +0800

    Update VKPhysicalDevice

--- Source/Vulkan/VKPhysicalDevice.cpp
+++ Source/Vulkan/VKPhysicalDevice.cpp
@@ -5,14 +5,17 @@
 #include <vector>
 
 VKPhysicalDevice::VKPhysicalDevice(VkPhysicalDevice device, VkSurfaceKHR surface)
-	: handle(device)
+	: handle(device), surface(surface)
 {
+	vkGetPhysicalDeviceProperties(device, &properties);
+	vkGetPhysicalDeviceFeatures(device, &features);
+
+	// 获取队列族索引
 	uint32_t queueFamilyCount;
 	vkGetPhysicalDeviceQueueFamilyProperties(handle, &queueFamilyCount, nullptr);
 	std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
 	vkGetPhysicalDeviceQueueFamilyProperties(handle, &queueFamilyCount, queueFamilies.data());
-
-	for(size_t i = 0; i < queueFamilies.size(); i++)
+	for(uint32_t i = 0; i < queueFamilies.size(); i++)
 	{
 		if(queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
 			graphics = i;
@@ -26,6 +29,54 @@ VKPhysicalDevice::VKPhysicalDevice(VkPhysicalDevice device, VkSurfaceKHR surface
 		if(presentSupport)
 			present = i;
 	}
+
+	uint32_t extensionCount;
+	vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, nullptr);
+	availableExtensions.resize(extensionCount);
+	vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, availableExtensions.data());
+
+	vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, &capabilities);
+
+	std::vector<VkSurfaceFormatKHR> formats;
+	uint32_t formatCount;
+	vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, nullptr);
+	if(formatCount != 0)
+	{
+		formats.resize(formatCount);
+		vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, formats.data());
+	}
+
+	std::vector<VkPresentModeKHR> presentModes;
+	uint32_t presentModeCount;
+	vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, nullptr);
+	if(presentModeCount != 0)
+	{
+		presentModes.resize(presentModeCount);
+		vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, presentModes.data());
+	}
+}
+
+const VkPhysicalDeviceProperties& VKPhysicalDevice::getProperties() const
+{
+	return properties;
+}
+
+const VkPhysicalDeviceFeatures& VKPhysicalDevice::getFeatures() const
+{
+	return features;
+}
+
+bool VKPhysicalDevice::isExtensionAvailable(std::string_view name) const
+{
+	for(const auto& ext : availableExtensions)
+		if(ext.extensionName == name)
+			return true;
+	return false;
+}
+
+VkSurfaceKHR VKPhysicalDevice::getSurface() const
+{
+	return surface;
 }
 
 VKPhysicalDevice::operator VkPhysicalDevice() const
--- Source/Vulkan/VKPhysicalDevice.h
+++ Source/Vulkan/VKPhysicalDevice.h
@@ -4,11 +4,21 @@
 #pragma once
 
 #include <vulkan/vulkan.h>
+#include <string_view>
+#include <vector>
 
 class VKPhysicalDevice final
 {
 public:
 	VKPhysicalDevice(VkPhysicalDevice device, VkSurfaceKHR surface);
+
+	const VkPhysicalDeviceProperties& getProperties() const;
+
+	const VkPhysicalDeviceFeatures& getFeatures() const;
+
+	VkSurfaceKHR getSurface() const;
+
+	bool isExtensionAvailable(std::string_view name) const;
 
 	uint32_t graphics = -1;
 	uint32_t compute = -1;
@@ -18,5 +28,12 @@ public:
 	operator VkPhysicalDevice() const;
 
 private:
-	VkPhysicalDevice handle;
+	VkPhysicalDevice           handle;
+	VkPhysicalDeviceProperties properties;
+	VkPhysicalDeviceFeatures   features;
+
+	VkSurfaceKHR surface;
+
+	VkSurfaceCapabilitiesKHR capabilities;
+	std::vector<VkExtensionProperties> availableExtensions;
 };
