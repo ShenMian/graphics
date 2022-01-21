e069b19f28904c6877041107cbf23bf41cabda19
ShenMian <ShenMian@users.noreply.github.com>
2022-01-21 15:23:45 +0000

    deploy: 1b8f0696f86fcb153c84444a0329d08e18ec366a

--- class_instance_builder.js
+++ class_instance_builder.js
@@ -2,7 +2,7 @@ var class_instance_builder =
 [
     [ "InstanceBuilder", "class_instance_builder.html#ae6070ddc2b28bad801ec4e2531ffc471", null ],
     [ "build", "class_instance_builder.html#ada89f89e11e084411bb82148049eb163", null ],
-    [ "enableDebugMessager", "class_instance_builder.html#ad87932bb2d37ee82a11b7b6bec49e7dc", null ],
+    [ "enableDebugMessenger", "class_instance_builder.html#aeab07698130fbad25302054954027fd1", null ],
     [ "enableExtension", "class_instance_builder.html#a0c832424d8b56ff50f66b0c96c801912", null ],
     [ "enableLayer", "class_instance_builder.html#a35caa59a89108e38049f843ba5c0a9a5", null ],
     [ "enableValidationLayers", "class_instance_builder.html#a7c7b5b81f59a7c7dd453a3101a752081", null ],
@@ -14,6 +14,6 @@ var class_instance_builder =
     [ "debugMessageCallback", "class_instance_builder.html#ae7b95751a10ee6dfbdb7b4ac827ba54b", null ],
     [ "debugMessageSeverity", "class_instance_builder.html#a5abc19d106f38494604b7e99ba3874bd", null ],
     [ "debugMessageType", "class_instance_builder.html#a813f161e80dc0b5a884b55d3feebd22c", null ],
-    [ "enableDebugMessager", "class_instance_builder.html#a2fbe2215f216a3c5a8f011f712bac2ae", null ],
+    [ "enableDebugMessager", "class_instance_builder.html#ae04115f8e6fd3e3f98b77e33d84e652a", null ],
     [ "enableValidationLayers", "class_instance_builder.html#a6742cf811e2cba1a6911bc67f773994a", null ]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['enabledebugmessager_314',['enableDebugMessager',['../class_instance_builder.html#ad87932bb2d37ee82a11b7b6bec49e7dc',1,'InstanceBuilder']]],
+  ['enabledebugmessenger_314',['enableDebugMessenger',['../class_instance_builder.html#aeab07698130fbad25302054954027fd1',1,'InstanceBuilder']]],
   ['enableextension_315',['enableExtension',['../class_instance_builder.html#a0c832424d8b56ff50f66b0c96c801912',1,'InstanceBuilder']]],
   ['enablelayer_316',['enableLayer',['../class_instance_builder.html#a35caa59a89108e38049f843ba5c0a9a5',1,'InstanceBuilder']]],
   ['enablevalidationlayers_317',['enableValidationLayers',['../class_instance_builder.html#a7c7b5b81f59a7c7dd453a3101a752081',1,'InstanceBuilder']]]
