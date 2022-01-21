689d19d7a4c82e0b90cd7f17b8b7959e04720b3b
ShenMian <sms_school@outlook.com>
2022-01-22 01:06:11 +0800

    Update VKSwapchain

--- Source/Vulkan/VKSwapchain.cpp
+++ Source/Vulkan/VKSwapchain.cpp
@@ -1,12 +1,49 @@
-// Copyright 2021 ShenMian
+﻿// Copyright 2021 ShenMian
 // License(Apache-2.0)
 
 #include "VKSwapchain.h"
+#include <stdexcept>
 
-Swapchain::Swapchain(VkSurfaceKHR surface)
+VKSwapchain::VKSwapchain(VkSwapchainKHR swapchain, VKDevice& device, VkFormat imageFormat, VkExtent2D extent)
+	: handle(swapchain), device(device), imageFormat(imageFormat), extent(extent)
 {
-	VkSwapchainCreateInfoKHR info = {};
-	info.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
-	info.surface = surface;
+	uint32_t imageCount;
+	vkGetSwapchainImagesKHR(device, handle, &imageCount, nullptr);
+	images.resize(imageCount);
+	vkGetSwapchainImagesKHR(device, handle, &imageCount, images.data());
 
+	imageViews.resize(images.size());
+	for(size_t i = 0; i < images.size(); i++)
+	{
+		VkImageViewCreateInfo imageViewInfo = {};
+		imageViewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+		imageViewInfo.image = images[i];
+
+		imageViewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+		imageViewInfo.format = imageFormat;
+
+		imageViewInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+		imageViewInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+		imageViewInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+		imageViewInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+		imageViewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+		imageViewInfo.subresourceRange.baseMipLevel = 0;
+		imageViewInfo.subresourceRange.levelCount = 1;
+		imageViewInfo.subresourceRange.baseArrayLayer = 0;
+		imageViewInfo.subresourceRange.layerCount = 1;
+
+		if(vkCreateImageView(device, &imageViewInfo, nullptr, &imageViews[i]) != VK_SUCCESS)
+			throw std::runtime_error("failed to create image view");
+	}
+}
+
+VKSwapchain::operator VkSwapchainKHR()
+{
+	return handle;
+}
+
+VKSwapchain::operator VkSwapchainKHR() const
+{
+	return handle;
 }
--- Source/Vulkan/VKSwapchain.h
+++ Source/Vulkan/VKSwapchain.h
@@ -1,14 +1,27 @@
-// Copyright 2021 ShenMian
+﻿// Copyright 2021 ShenMian
 // License(Apache-2.0)
 
 #pragma once
 
+#include "VKDevice.h"
 #include <vulkan/vulkan.h>
+#include <vector>
 
-class Swapchain
+class VKSwapchain
 {
 public:
-	Swapchain(VkSurfaceKHR surface);
+	VKSwapchain() = default;
+	VKSwapchain(VkSwapchainKHR swapchain, VKDevice& device, VkFormat imageFormat, VkExtent2D extent);
+
+	operator VkSwapchainKHR();
+	operator VkSwapchainKHR() const;
 
 private:
+	VkSwapchainKHR handle;
+	VKDevice       device;
+	VkFormat       imageFormat;
+	VkExtent2D     extent;
+
+	std::vector<VkImage>     images;
+	std::vector<VkImageView> imageViews;
 };
