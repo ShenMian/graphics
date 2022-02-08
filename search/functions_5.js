8c208396bfafeaaf87f45697a552bc0829de7da1
ShenMian <ShenMian@users.noreply.github.com>
2022-02-08 10:05:32 +0000

    deploy: 5a8cd42c34383a928d3380861121b95a4387cbed

--- search/functions_5.js
+++ search/functions_5.js
@@ -1,9 +1,10 @@
 var searchData=
 [
   ['image_0',['Image',['../class_image.html#a5502e928122744be1d3b89648298e162',1,'Image::Image()=default'],['../class_image.html#aaf8eb0773fa6f75d832d5f7bb45ec8de',1,'Image::Image(const std::filesystem::path &amp;path)']]],
-  ['isconnected_1',['isConnected',['../class_gamepad.html#a33d4dbd503a8d645885bd19cbc390a91',1,'Gamepad']]],
-  ['isfullscreen_2',['isFullscreen',['../class_window.html#ac3dc6c7f08d4b749b7949149cc6c7dc3',1,'Window']]],
-  ['ispressed_3',['isPressed',['../class_input.html#a77ca3703c1457f69fa12c89f5b08d917',1,'Input::isPressed(Key key)'],['../class_input.html#adfeb39c4544f18847f3b7353fec5ef2d',1,'Input::isPressed(Mouse key)']]],
-  ['isprimary_4',['isPrimary',['../class_monitor.html#abb69747f03e4fed060f16b84052eb3c0',1,'Monitor']]],
-  ['isvisible_5',['isVisible',['../class_window.html#a6be53a737e7dfcc74167962437e93b8f',1,'Window']]]
+  ['init_1',['init',['../class_g_l_renderer.html#ad419c85c7b9ede2f3a21f1e7fe3231ff',1,'GLRenderer']]],
+  ['isconnected_2',['isConnected',['../class_gamepad.html#a33d4dbd503a8d645885bd19cbc390a91',1,'Gamepad']]],
+  ['isfullscreen_3',['isFullscreen',['../class_window.html#ac3dc6c7f08d4b749b7949149cc6c7dc3',1,'Window']]],
+  ['ispressed_4',['isPressed',['../class_input.html#a77ca3703c1457f69fa12c89f5b08d917',1,'Input::isPressed(Key key)'],['../class_input.html#adfeb39c4544f18847f3b7353fec5ef2d',1,'Input::isPressed(Mouse key)']]],
+  ['isprimary_5',['isPrimary',['../class_monitor.html#abb69747f03e4fed060f16b84052eb3c0',1,'Monitor']]],
+  ['isvisible_6',['isVisible',['../class_window.html#a6be53a737e7dfcc74167962437e93b8f',1,'Window']]]
 ];
