aabdbdca1b3089642001fb06895dfd5296ff7a29
ShenMian <sms_school@outlook.com>
2022-02-13 16:32:45 +0800

    Update premake

--- Examples/Test/premake5.lua
+++ Examples/Test/premake5.lua
@@ -18,8 +18,8 @@ project "test"
 		"%{deps_inc.glad}",
 		"%{deps_inc.math}",
 		"%{deps_inc.imgui}",
-		"%{deps_inc.imgui_node_editor}",
-		"%{deps_inc.vulkan}"}
+		"%{deps_inc.vulkan}",
+		"%{deps_inc.imgui_node_editor}"}
 
 	links "Graphics"
 
--- Examples/Triangle/premake5.lua
+++ Examples/Triangle/premake5.lua
@@ -14,10 +14,12 @@ project "triangle"
 
 	includedirs {
 		"%{wks.location}/Source",
+		"%{deps_inc.fmt}",
+		"%{deps_inc.glad}",
 		"%{deps_inc.math}",
 		"%{deps_inc.imgui}",
-		"%{deps_inc.imgui_node_editor}",
-		"%{deps_inc.vulkan}"}
+		"%{deps_inc.vulkan}",
+		"%{deps_inc.imgui_node_editor}"}
 
 	links "Graphics"
 
