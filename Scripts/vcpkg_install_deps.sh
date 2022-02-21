485ad9dd55d687bd07f3d8b758d37f6057178a0d
ShenMian <sms_school@outlook.com>
2022-02-22 01:13:47 +0800

    Add vcpkg_install_deps.sh Install dependencies by vcpkg.

---
+++ Scripts/vcpkg_install_deps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright 2021 ShenMian
+# License(Apache-2.0)
+
+# 獲取管理員權限
+if ! sudo echo
+then
+    echo "Can not get sudo permission."
+    exit 1
+fi
+
+echo Installing dependencies...
+sudo vcpkg install assimp glfw3 meshoptimizer
