90b78fcf8c54af81c621a9d693165489006a9ed3
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-03-01 20:36:01 +0800

    Update Buffer.h

--- Source/Buffer.h
+++ Source/Buffer.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include <cstddef>
+#include <span>
 
 /**
  * @brief 缓冲区.
@@ -40,6 +41,27 @@ public:
 	 */
 	Buffer(size_t size, Type type, Usage usage);
 
+    template <typename T>
+    void write(std::span<T> data, size_t offset = 0);
+
+    /**
+     * @brief 写入映射缓冲区.
+     *
+     * @param data   要写入的数据.
+     * @param size   要写入的大小, 单位: byte.
+     * @param offset 偏移量, 单位: byte.
+     */
+    void write(const void* data, size_t size, size_t offset = 0);
+
+    /**
+     * @brief 读取映射缓冲区.
+     *
+     * @param data   要写入的缓冲区.
+     * @param size   要读取的大小, 单位: byte.
+     * @param offset 偏移量, 单位: byte.
+     */
+    void read(void* data, size_t size, size_t offset = 0);
+
 	/**
 	 * @brief 获取缓冲区大小, 单位: 字节.
 	 */
@@ -59,24 +81,6 @@ public:
 	 * @brief 获取映射的缓冲区.
 	 */
     [[nodiscard]] void* getData();
-
-	/**
-	 * @brief 写入映射缓冲区.
-	 *
-	 * @param data   要写入的数据.
-	 * @param size   要写入的大小, 单位: byte.
-	 * @param offset 偏移量, 单位: byte.
-	 */
-	void write(const void* data, size_t size, size_t offset = 0);
-
-	/**
-	 * @brief 读取映射缓冲区.
-	 *
-	 * @param data   要写入的缓冲区.
-	 * @param size   要读取的大小, 单位: byte.
-	 * @param offset 偏移量, 单位: byte.
-	 */
-	void read(void* data, size_t size, size_t offset = 0);
 
 	/**
 	 * @brief 映射缓冲区到内存.
@@ -105,3 +109,9 @@ protected:
 	Usage  usage;
 	void* data = nullptr;
 };
+
+template<typename T>
+void Buffer::write(std::span<T> data, size_t offset)
+{
+    write(data.data(), data.size_bytes(), offset);
+}
