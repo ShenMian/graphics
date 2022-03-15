c21096826f9e98218761d56164e4b3f4c9fdba43
ShenMian <ShenMian@users.noreply.github.com>
2022-03-15 13:29:58 +0000

    deploy: 044815f39c49ea176705be33b9f04f0daa07d13c

--- search/all_15.js
+++ search/all_15.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['warp_0',['Warp',['../class_texture.html#af110c1b8fc58036b94dced17b5399b68',1,'Texture']]],
   ['widget_1',['Widget',['../classui_1_1_widget.html',1,'ui']]],
-  ['window_2',['Window',['../classui_1_1_window.html',1,'ui::Window'],['../class_window.html',1,'Window'],['../class_window.html#a4833e71f6901fa71d1d829b5d551e118',1,'Window::Window()']]],
+  ['window_2',['Window',['../classui_1_1_window.html',1,'ui::Window'],['../class_window.html',1,'Window'],['../class_window.html#a147796917726708be2db0b7c381368a9',1,'Window::Window()']]],
   ['write_3',['write',['../class_buffer.html#a1dfdf1f77eeec6e076bba544fd7a9b82',1,'Buffer']]]
 ];
--- search/functions_c.js
+++ search/functions_c.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['window_0',['Window',['../class_window.html#a4833e71f6901fa71d1d829b5d551e118',1,'Window']]],
+  ['window_0',['Window',['../class_window.html#a147796917726708be2db0b7c381368a9',1,'Window']]],
   ['write_1',['write',['../class_buffer.html#a1dfdf1f77eeec6e076bba544fd7a9b82',1,'Buffer']]]
 ];
