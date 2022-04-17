843b3404748d8dc35ba63f9d7e25b04a84932705
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-04-17 20:58:59 +0800

    Add Animation

---
+++ Source/Animation.cpp
@@ -0,0 +1,24 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#include "Animation.h"
+
+Animation::Animation(const std::string& name, float duration, uint32_t tickRate)
+	: name(name), duration(duration), tickRate(tickRate)
+{
+}
+
+const std::string& Animation::getName() const
+{
+	return name;
+}
+
+float Animation::getDuration() const
+{
+	return duration;
+}
+
+uint32_t Animation::getTickRate() const
+{
+	return tickRate;
+}
---
+++ Source/Animation.h
@@ -0,0 +1,32 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#pragma once
+
+#include <string>
+
+/**
+ * @brief 3D 动画.
+ */
+class Animation
+{
+public:
+	Animation(const std::string& name, float duration, uint32_t tickRate);
+
+	/**
+	 * @brief 获取名称.
+	 */
+	const std::string& getName() const;
+
+	/**
+	 * @brief 获取持续时间, 单位: 秒.
+	 */
+	float getDuration() const;
+
+	uint32_t getTickRate() const;
+
+private:
+	std::string name;
+	float       duration;
+	uint32_t    tickRate;
+};
