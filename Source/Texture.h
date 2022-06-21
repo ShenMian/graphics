ac8c81857124e15ff0b586e4b2a29229b659e8d2
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-06-21 15:57:06 +0800

    Update std::hash usage

--- Source/Texture.h
+++ Source/Texture.h
@@ -122,16 +122,14 @@ protected:
 	static std::unordered_map<std::filesystem::path, std::shared_ptr<Texture>> cache;
 };
 
+// TODO: 需要移动到合适的位置
 #if TARGET_COMPILER == COMPILER_CLANG
-namespace std
-{
 template <>
-struct hash<std::filesystem::path>
+struct std::hash<std::filesystem::path>
 {
 	size_t operator()(const std::filesystem::path& path) const noexcept
 	{
 		return std::filesystem::hash_value(path);
 	}
 };
-} // namespace std
 #endif
