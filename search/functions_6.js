45c83c52716c5e8b225297d8356ba97be6498266
ShenMian <ShenMian@users.noreply.github.com>
2022-07-06 16:29:48 +0000

    deploy: 6cacbdf9fbf5be463a8ecd4b3bf31c8dae561f12

--- search/functions_6.js
+++ search/functions_6.js
@@ -3,10 +3,11 @@ var searchData=
   ['image_0',['Image',['../class_image.html#a5502e928122744be1d3b89648298e162',1,'Image::Image()=default'],['../class_image.html#aaf8eb0773fa6f75d832d5f7bb45ec8de',1,'Image::Image(const std::filesystem::path &amp;path)'],['../class_image.html#a0d2dc69e9a91f36211469db17036fb44',1,'Image::Image(const void *data, size_t sizeBytes, Vector2i size, int channels)']]],
   ['init_1',['init',['../class_g_l_renderer.html#ad419c85c7b9ede2f3a21f1e7fe3231ff',1,'GLRenderer']]],
   ['isconnected_2',['isConnected',['../class_gamepad.html#a33d4dbd503a8d645885bd19cbc390a91',1,'Gamepad']]],
-  ['isfloating_3',['isFloating',['../class_window.html#a749973cb62e3321d7cbfe4ff39798189',1,'Window']]],
-  ['isfullscreen_4',['isFullscreen',['../class_window.html#ac3dc6c7f08d4b749b7949149cc6c7dc3',1,'Window']]],
-  ['ispressed_5',['isPressed',['../class_input.html#a77ca3703c1457f69fa12c89f5b08d917',1,'Input::isPressed(Key key)'],['../class_input.html#adfeb39c4544f18847f3b7353fec5ef2d',1,'Input::isPressed(Mouse key)']]],
-  ['isprimary_6',['isPrimary',['../class_monitor.html#abb69747f03e4fed060f16b84052eb3c0',1,'Monitor']]],
-  ['isresizable_7',['isResizable',['../class_window.html#a1b888a7e6a5562968a253b915ccb7825',1,'Window']]],
-  ['isvisible_8',['isVisible',['../class_window.html#a6be53a737e7dfcc74167962437e93b8f',1,'Window']]]
+  ['iscursorlock_3',['isCursorLock',['../class_window.html#ad07aacec09f143cbf9acf69a79764bbe',1,'Window']]],
+  ['isfloating_4',['isFloating',['../class_window.html#a749973cb62e3321d7cbfe4ff39798189',1,'Window']]],
+  ['isfullscreen_5',['isFullscreen',['../class_window.html#ac3dc6c7f08d4b749b7949149cc6c7dc3',1,'Window']]],
+  ['ispressed_6',['isPressed',['../class_input.html#a77ca3703c1457f69fa12c89f5b08d917',1,'Input::isPressed(Key key)'],['../class_input.html#adfeb39c4544f18847f3b7353fec5ef2d',1,'Input::isPressed(Mouse key)']]],
+  ['isprimary_7',['isPrimary',['../class_monitor.html#abb69747f03e4fed060f16b84052eb3c0',1,'Monitor']]],
+  ['isresizable_8',['isResizable',['../class_window.html#a1b888a7e6a5562968a253b915ccb7825',1,'Window']]],
+  ['isvisible_9',['isVisible',['../class_window.html#a6be53a737e7dfcc74167962437e93b8f',1,'Window']]]
 ];
