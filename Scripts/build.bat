c463cd307437369eab631024235ca87eb272165b
ShenMian <sms_school@outlook.com>
2022-07-14 23:43:11 +0800

    Update scripts

--- Scripts/build.bat
+++ Scripts/build.bat
@@ -2,30 +2,39 @@
 REM Copyright 2021 ShenMian
 REM License(Apache-2.0)
 
+cmake --version >nul 2>&1 || (
+    echo === Need cmake
+    exit /b 1
+)
+
+if "%~1"=="" (set "BUILD_TYPE=Debug") else (set "BUILD_TYPE=%~1")
+if "%BUILD_TYPE%"=="Debug" (set "VS_ARGS=MTd") else (set "VS_ARGS=MT")
+
 pushd %~dp0\..
 
-echo Installing dependencies...
+echo === Installing dependencies...
 mkdir build 2>nul
-pushd  build
-set CONAN_SYSREQUIRES_MODE=enabled
-conan install .. -s compiler.runtime=MDd --build=missing >nul
+pushd build
+
+set "CONAN_SYSREQUIRES_MODE=enabled"
+conan install .. --build=missing -s build_type=%BUILD_TYPE% -s compiler.runtime=%VS_ARGS% >nul
+
 popd
 
-echo Generating CMake cache...
-cmake -B build -DUSE_CONAN=ON >nul || (
-    echo Failed to generate CMake cache.
-    cmake -B build -DUSE_CONAN=ON
+echo === Generating CMake cache...
+cmake -B build >nul || (
+    echo === Failed to generate CMake cache.
+    cmake -B build
     exit /b 1
 )
 
-echo Building...
+echo === Building...
 cmake --build build >nul || (
-    echo Failed to build.
-    cmake --build build -j16
+    echo === Failed to build.
+    cmake --build build --config %BUILD_TYPE% -j16
     exit /b 1
 )
 
-echo Done.
+echo === Done.
 
 popd
-
--- Scripts/build.sh
+++ Scripts/build.sh
@@ -2,31 +2,41 @@
 # Copyright 2021 ShenMian
 # License(Apache-2.0)
 
+if ! hash "cmake" &>/dev/null; then
+    echo === Need cmake
+    exit 1
+fi
+
+if [ $# -eq 0 ]
+    BUILD_TYPE="Debug"
+else
+    BUILD_TYPE=$1
+fi
+
 cd "$( cd "$( dirname "$0"  )" && pwd  )" || exit
 cd .. || exit
 
-echo Installing dependencies...
+echo === Installing dependencies...
 mkdir build 2>/dev/null
 cd build
 export CONAN_SYSREQUIRES_MODE=enabled
-conan install .. --build=missing >/dev/null
+conan install .. --build=missing -s build_type=${BUILD_TYPE} >/dev/null
 cd .. || exit
 
-echo Generating CMake cache...
-if ! cmake -B build -DUSE_CONAN=ON >/dev/null
+echo === Generating CMake cache...
+if ! cmake -B build >/dev/null
 then
-    echo Failed to generate CMake cache.
-    cmake -B build -DUSE_CONAN=ON
+    echo === Failed to generate CMake cache.
+    cmake -B build
     exit 1
 fi
 
-echo Building...
+echo === Building...
 if ! cmake --build build >/dev/null
 then
-    echo Failed to build.
-    cmake --build build -j16
+    echo === Failed to build.
+    cmake --build build --config ${BUILD_TYPE} -j16
     exit 1
 fi
 
-echo Done.
-
+echo === Done.
