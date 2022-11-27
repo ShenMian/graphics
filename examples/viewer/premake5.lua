9740b67d70ca4418a0a5f03ed5a9e5f513607d37
ShenMian <sms_school@outlook.com>
2022-11-27 20:06:51 +0800

    refactor: rename files

--- examples/viewer/premake5.lua
+++ examples/viewer/premake5.lua
@@ -1,26 +1,27 @@
 ﻿-- Copyright 2021 SMS
 -- License(Apache-2.0)
 
-project "viewer"
-	kind "ConsoleApp"
-	language "C++"
-	cppdialect "C++latest"
-	staticruntime "on"
+project("viewer")
+kind("ConsoleApp")
+language("C++")
+cppdialect("C++latest")
+staticruntime("on")
 
-	targetdir("%{wks.location}/build/" .. output_dir .. "/%{prj.name}/bin")
-	objdir("%{wks.location}/build/" .. output_dir .. "/%{prj.name}/obj")
+targetdir("%{wks.location}/build/" .. output_dir .. "/%{prj.name}/bin")
+objdir("%{wks.location}/build/" .. output_dir .. "/%{prj.name}/obj")
 
-	files {"**.cpp", "**.hpp", "**.h", "**.inl", "CMakeLists.txt", "premake5.lua", "Shaders/**"}
+files({ "**.cpp", "**.hpp", "**.h", "**.inl", "CMakeLists.txt", "premake5.lua", "Shaders/**" })
 
-	includedirs {
-		"%{wks.location}/Source",
-		"%{deps_inc.fmt}",
-		"%{deps_inc.glad}",
-		"%{deps_inc.math}",
-		"%{deps_inc.imgui}",
-		"%{deps_inc.vulkan}",
-		"%{deps_inc.imgui_node_editor}"}
+includedirs({
+	"%{wks.location}/src",
+	"%{deps_inc.fmt}",
+	"%{deps_inc.glad}",
+	"%{deps_inc.math}",
+	"%{deps_inc.imgui}",
+	"%{deps_inc.vulkan}",
+	"%{deps_inc.imgui_node_editor}",
+})
 
-	links "Graphics"
+links("Graphics")
 
-	postbuildcommands "{COPYDIR} \"Shaders\" \"%{cfg.targetdir}\""
+postbuildcommands('{COPYDIR} "Shaders" "%{cfg.targetdir}"')
--- premake5.lua
+++ premake5.lua
@@ -1,98 +1,99 @@
 ﻿-- Copyright 2021 SMS
 -- License(Apache-2.0)
 
-include "deps/premake/solution_items.lua"
+include("deps/premake/solution_items.lua")
 
-workspace "Graphics"
-    architecture "x86_64"
-    startproject "Triangle"
-    configurations {"Debug", "Release"}
-    flags "MultiProcessorCompile"
+workspace("Graphics")
+architecture("x86_64")
+startproject("Triangle")
+configurations({ "Debug", "Release" })
+flags("MultiProcessorCompile")
 
-    output_dir = "%{cfg.system}-%{cfg.architecture}-%{cfg.buildcfg}"
+output_dir = "%{cfg.system}-%{cfg.architecture}-%{cfg.buildcfg}"
 
-    configurations {"Debug", "Release"}
+configurations({ "Debug", "Release" })
 
-    filter "configurations:Debug"
-        defines "DEBUG"
-        runtime "Debug"
-        symbols "on"
+filter("configurations:Debug")
+defines("DEBUG")
+runtime("Debug")
+symbols("on")
 
-    filter "configurations:Release"
-        defines "NDEBUG"
-        runtime "Release"
-        optimize "on"
+filter("configurations:Release")
+defines("NDEBUG")
+runtime("Release")
+optimize("on")
 
-    filter "system:linux"
-        linkoptions "-pthread"
+filter("system:linux")
+linkoptions("-pthread")
 
-    solution_items {
-        ".editorconfig",
-        "README.md",
-        "CMakeLists.txt",
-        "premake5.lua"}
+solution_items({
+	".editorconfig",
+	"README.md",
+	"CMakeLists.txt",
+	"premake5.lua",
+})
 
-    -- 启用 OpenMP
-    -- openmp "On"
-    -- filter "toolset:not msc*"
-    --     buildoptions "-fopenmp"
+-- 启用 OpenMP
+-- openmp "On"
+-- filter "toolset:not msc*"
+--     buildoptions "-fopenmp"
 
-    -- 第三方库路径
-    deps = {}
-    deps["fmt"]               = "%{wks.location}/deps/fmt"
-    deps["stb"]               = "%{wks.location}/deps/stb"
-    deps["math"]              = "%{wks.location}/deps/Math"
-    deps["glad"]              = "%{wks.location}/deps/glad"
-    deps["glfw"]              = "%{wks.location}/deps/glfw"
-    deps["imgui"]             = "%{wks.location}/deps/imgui"
-    deps["assimp"]            = "%{wks.location}/deps/assimp"
-    deps["shaderc"]           = "%{wks.location}/deps/shaderc"
-    deps["glslang"]           = "%{wks.location}/deps/glslang"
-    deps["imguizmo"]          = "%{wks.location}/deps/ImGuizmo"
-    deps["spirv_tools"]       = "%{wks.location}/deps/SPIRV-Tools"
-    deps["vk_bootstrap"]      = "%{wks.location}/deps/vk-bootstrap"
-    deps["spirv_headers"]     = "%{wks.location}/deps/SPIRV-Headers"
-    deps["meshoptimizer"]     = "%{wks.location}/deps/meshoptimizer"
-    deps["imgui_node_editor"] = "%{wks.location}/deps/imgui-node-editor"
+-- 第三方库路径
+deps = {}
+deps["fmt"] = "%{wks.location}/deps/fmt"
+deps["stb"] = "%{wks.location}/deps/stb"
+deps["math"] = "%{wks.location}/deps/Math"
+deps["glad"] = "%{wks.location}/deps/glad"
+deps["glfw"] = "%{wks.location}/deps/glfw"
+deps["imgui"] = "%{wks.location}/deps/imgui"
+deps["assimp"] = "%{wks.location}/deps/assimp"
+deps["shaderc"] = "%{wks.location}/deps/shaderc"
+deps["glslang"] = "%{wks.location}/deps/glslang"
+deps["imguizmo"] = "%{wks.location}/deps/ImGuizmo"
+deps["spirv_tools"] = "%{wks.location}/deps/SPIRV-Tools"
+deps["vk_bootstrap"] = "%{wks.location}/deps/vk-bootstrap"
+deps["spirv_headers"] = "%{wks.location}/deps/SPIRV-Headers"
+deps["meshoptimizer"] = "%{wks.location}/deps/meshoptimizer"
+deps["imgui_node_editor"] = "%{wks.location}/deps/imgui-node-editor"
 
-    -- 第三方库头文件路径
-    deps_inc = {}
-    deps_inc["fmt"]               = "%{deps.fmt}/include"
-    deps_inc["stb"]               = "%{deps.stb}"
-    deps_inc["math"]              = "%{deps.math}/include"
-    deps_inc["glad"]              = "%{deps.glad}/include"
-    deps_inc["glfw"]              = "%{deps.glfw}/include"
-    deps_inc["imgui"]             = "%{deps.imgui}"
-    deps_inc["assimp"]            = "%{deps.assimp}/include"
-    deps_inc["vulkan"]            = "%{wks.location}/deps/Vulkan-Headers/include"
-    deps_inc["imguizmo"]          = "%{wks.location}/deps/ImGuizmo"
-    deps_inc["vk_bootstrap"]      = "%{deps.vk_bootstrap}/src"
-    deps_inc["meshoptimizer"]     = "%{deps.meshoptimizer}/src"
-    deps_inc["imgui_node_editor"] = "%{deps.imgui_node_editor}"
-    -- deps_inc["shaderc"] = "%{deps.shaderc}/libshaderc/include"
+-- 第三方库头文件路径
+deps_inc = {}
+deps_inc["fmt"] = "%{deps.fmt}/include"
+deps_inc["stb"] = "%{deps.stb}"
+deps_inc["math"] = "%{deps.math}/include"
+deps_inc["glad"] = "%{deps.glad}/include"
+deps_inc["glfw"] = "%{deps.glfw}/include"
+deps_inc["imgui"] = "%{deps.imgui}"
+deps_inc["assimp"] = "%{deps.assimp}/include"
+deps_inc["vulkan"] = "%{wks.location}/deps/Vulkan-Headers/include"
+deps_inc["imguizmo"] = "%{wks.location}/deps/ImGuizmo"
+deps_inc["vk_bootstrap"] = "%{deps.vk_bootstrap}/src"
+deps_inc["meshoptimizer"] = "%{deps.meshoptimizer}/src"
+deps_inc["imgui_node_editor"] = "%{deps.imgui_node_editor}"
+-- deps_inc["shaderc"] = "%{deps.shaderc}/libshaderc/include"
 
-    deps_lib = {}
-    deps_lib["vulkan"] = "%{wks.location}/deps/VulkanSDK/Lib/vulkan-1"
+deps_lib = {}
+deps_lib["vulkan"] = "%{wks.location}/deps/VulkanSDK/Lib/vulkan-1"
 
-    include "source"
+include("src")
 
-    group "examples"
-        include "Examples/Triangle"
-        include "Examples/Cube"
-        include "Examples/Viewer"
-    group ""
+group("examples")
+include("Examples/Triangle")
+include("Examples/Cube")
+include("Examples/Viewer")
+group("")
 
-    group "deps"
-        include "deps/glad"
-        include "deps/glfw"
-        include "deps/imgui"
-        include "deps/assimp"
-        include "deps/imguizmo"
-        include "deps/vk_bootstrap"
-        include "deps/meshoptimizer"
-        include "deps/imgui_node_editor"
-        -- include "deps/shaderc"
-        -- include "deps/glslang"
-        -- include "deps/spirv_tools"
-        -- include "deps/spirv_cross"
-    group ""
+group("deps")
+include("deps/glad")
+include("deps/glfw")
+include("deps/imgui")
+include("deps/assimp")
+include("deps/imguizmo")
+include("deps/vk_bootstrap")
+include("deps/meshoptimizer")
+include("deps/imgui_node_editor")
+-- include "deps/shaderc"
+-- include "deps/glslang"
+-- include "deps/spirv_tools"
+-- include "deps/spirv_cross"
+group("")
